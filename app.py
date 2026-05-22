6ac2dff278ee767fdd555843791115d163aefc23
siwa-hash <siwanut.sr@gmail.com>
2026-05-22 15:59:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ st.title("🏠 หน้าหลัก ")
 st.write("### Boot Camp: Data Science and Machine Learning")
 st.info("7 Day Intensive Hands-on Workshop")
 st.write("nut-ss")
+st.mrakdown(''':rainbow[nut-ss]''')
 st.write("##### Day 1: การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
 
 if st.button("💰 ระบบคำนวณส่วนลดตามยอดซื้อ"):
